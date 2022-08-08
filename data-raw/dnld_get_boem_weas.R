39f5ef0939e8505b65f083487ebe3f1674e92960
jmhatch <jmhatch@vt.edu>
2022-08-08 13:06:11 -0400

    Test GH action

--- data-raw/dnld_get_boem_weas.R
+++ data-raw/dnld_get_boem_weas.R
@@ -35,22 +35,26 @@ sf::sf_use_s2(FALSE) # turn off s2 processing
 
 # function to extract WEAs
 get_boem_weas <- function(gdb_loc, save_clean = TRUE) {
-
+  
   # list layers in gdb
   list_layers <- here::here(paste0(gdb_loc, '/BOEMWindLayers_4Download.gdb')) %>% sf::st_layers()
-
+  
   # grab layer names
   active_name <- list_layers$name[stringr::str_detect(list_layers$name, 'WindLeaseOutlines')]
   planning_name <- list_layers$name[stringr::str_detect(list_layers$name, 'WindPlanningAreaOutlines')]
-    
+  
+  # extract dates
+  active_date <- paste(stringr::str_split(active_name, pattern = '_')[[1]][c(4,2,3)], collapse = '/')
+  planning_date <- paste(stringr::str_split(planning_name, pattern = '_')[[1]][c(4,2,3)], collapse = '/')
+  
   # read in feature layers
   active_shapes <- here::here(paste0(gdb_loc, '/BOEMWindLayers_4Download.gdb')) %>% 
     sf::st_read(layer = active_name) %>%
-    dplyr::mutate(LEASE_STAGE = 'Active')
+    dplyr::mutate(LEASE_STAGE = 'Active', UPDATED = active_date)
   planning_shapes <- here::here(paste0(gdb_loc, '/BOEMWindLayers_4Download.gdb')) %>% 
     sf::st_read(layer = planning_name) %>%
-    dplyr::mutate(LEASE_STAGE = 'Planning')
-    
+    dplyr::mutate(LEASE_STAGE = 'Planning', UPDATED = planning_date)
+  
   # figure out which columns are in one and not the other
   cols_not_planning = colnames(active_shapes)[!colnames(active_shapes) %in% colnames(planning_shapes)]
   cols_not_active = colnames(planning_shapes)[!colnames(planning_shapes) %in% colnames(active_shapes)]
@@ -58,7 +62,7 @@ get_boem_weas <- function(gdb_loc, save_clean = TRUE) {
   # add missing columns to enable rbind
   active_shapes[cols_not_active] = NA
   planning_shapes[cols_not_planning] = NA 
-    
+  
   # combine 
   boem_wea_outlines <- sf:::rbind.sf(active_shapes, planning_shapes)
   
@@ -70,49 +74,6 @@ get_boem_weas <- function(gdb_loc, save_clean = TRUE) {
     usethis::use_data(boem_wea_outlines, overwrite = TRUE)
   } else {
     return(boem_wea_outlines)
-  }
-  
-}
-
-#' Was the metadata updated for renewable energy lease areas and wind planning areas from BOEM?
-#' 
-#' @return TRUE if updated, otherwise FALSE.
-#' 
-weas_updated = function() {
-  
-  # download html from boem website
-  wea_html <- 'https://catalog.data.gov/dataset/boem-wind-planning-areas' %>% 
-    rvest::read_html() 
-  
-  # html nodes with links
-  metadata_links <- wea_html %>% 
-    rvest::html_nodes(css = '[href]') 
-  
-  # look for a specific node
-  metadata_href <- tibble::tibble(links = metadata_links %>% rvest::html_attr('href')) %>% 
-    tibble::rownames_to_column() %>%
-    dplyr::filter(links == '#sec-dates') %>% 
-    `[[`("rowname") %>%
-    as.numeric()
-   
-  # extract metadata update date
-  metadata_update <- metadata_links[metadata_href] %>% 
-    rvest::html_text()
-  
-  # read date of latest download
-  latest_date <- here::here('data-raw', 'boem-renewable-energy-geodatabase', 'boem_wea_date_downloaded.txt') %>%
-    readLines() %>%
-    as.Date(format = '%B %d, %Y')
-  
-  # was the geodatabase updated?
-  if (metadata_update %>% as.Date(format = '%B %d, %Y') > latest_date) {
-    
-    # save new date
-    cat(paste0(metadata_update, '\n'), file = here::here('data-raw', 'boem-renewable-energy-geodatabase', 'boem_wea_date_downloaded.txt'))
-
-    return(TRUE)
-  } else {
-    return(FALSE)
   }
   
 }
@@ -134,19 +95,27 @@ update_weas_R <- function() {
   x_max <- round(bbox['xmax'], 4)
   y_min <- round(bbox['ymin'], 4)
   y_max <- round(bbox['ymax'], 4)
-    
+  active_update <- boem_wea_outlines %>% dplyr::filter(LEASE_STAGE == 'Active') %>% pull(UPDATED) %>% unique()
+  planning_update <- boem_wea_outlines %>% dplyr::filter(LEASE_STAGE == 'Planning') %>% pull(UPDATED) %>% unique()
+  
+  # query ArcGIS REST service
+  url = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer'
+  active_layer_info <- jsonlite::fromJSON(httr::content(httr::POST(paste0(url, '/0'), query = list(f = "json"), encode = "form", config = httr::config(ssl_verifypeer = FALSE)), as = "text"))
+  planning_layer_info <- jsonlite::fromJSON(httr::content(httr::POST(paste0(url, '/2'), query = list(f = "json"), encode = "form", config = httr::config(ssl_verifypeer = FALSE)), as = "text"))
+  
   # paste string
   txt_file <- paste0("#' @title BOEM Renewable Energy Lease Areas and Wind Planning Areas
 #'
 #' @description A \\code{sf} object containing the outlines for BOEM Renewable Energy Lease Areas (LEASE_STAGE = 'Active') and Wind Planning Areas (LEASE_STAGE = 'Planning').
 #'
-#' @format A \\code{sf} collection with ", n_features," features and ", n_fields," fields:
+#' @format A \\code{sf} collection with ", n_features," features and ", n_fields," fields.
 #' \\describe{
-#'   \\item{Geometry type}{MULTIPOLYGON}
-#'   \\item{Dimension}{XY}
+#'   \\item{Geometry type}{", sf::st_geometry_type(boem_wea_outlines) %>% unique() %>% paste(collapse = ', '),"}
+#'   \\item{Dimension}{", ifelse(all(sf::st_dimension(boem_wea_outlines) == 2), 'XY', 'UNDET'),"}
 #'   \\item{Bounding box}{xmin: ", x_min," ymin: ", y_min," xmax: ", x_max," ymax: ", y_max,"}
-#'   \\item{Geodetic CRS}{WGS 84}
-#'   \\item{source}{data-raw/boem-renewable-energy-geodatabase/BOEMWindLayers_4Download.gdb}
+#'   \\item{Geodetic CRS}{", sf::st_crs(boem_wea_outlines)$input,"}
+#'   \\item{Source}{data-raw/boem-renewable-energy-geodatabase/BOEMWindLayers_4Download.gdb}
+#'   \\item{Fields}{Field descriptions can be found \\href{https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer/layers}{here}.}
 #' }
 #' 
 #' @docType data
@@ -154,26 +123,26 @@ update_weas_R <- function() {
 #' @usage data('boem_wea_outlines')
 #' @keywords datasets
 #' @source \\url{https://www.boem.gov/renewable-energy/mapping-and-data/renewable-energy-gis-data}
+#' @details
+#' BOEM wind lease area outlines were updated on ", active_update," (version ", active_layer_info$currentVersion,") and BOEM wind planning area outlines were updated on ", planning_update," (version ", planning_layer_info$currentVersion, ").
+#' 
+#' There may be more up to date BOEM wind planning areas than those included in NEFSCspatial.
+#' It is recommended that you reach out to the Wind Team at the NEFSC (\\email{angela.silva@@noaa.gov}) to confirm.
 NULL")
-
+  
   # output
   cat(txt_file, file = here::here('R/data_boem_wea_outlines.R'))
   
 }
 
-# update if needed
-if (weas_updated()) {
-  
-  # download
-  dnld_boem_weas(gdb_loc = gdb_loc)
-  
-  # extract
-  get_boem_weas(gdb_loc = gdb_loc)
-  
-  # update R 
-  update_weas_R()
-  
-  # re-build documentation
-  devtools::document()
-  
-}
+# download
+dnld_boem_weas(gdb_loc = gdb_loc)
+
+# extract
+get_boem_weas(gdb_loc = gdb_loc)
+
+# update R 
+update_weas_R()
+
+# re-build documentation
+devtools::document()
